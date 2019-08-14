0f424aea31fb5d5979fade5d767eafd2399a6d86
Giulio Dellorbo <giulio.dellorbo@8x8.com>
2019-08-14 10:04:54 -0400

    added dependency on event-emitter-3

--- streamingPlayer.js
+++ streamingPlayer.js
@@ -3,14 +3,14 @@ import {
   DeviceEventEmitter,
   Platform
 } from 'react-native';
-import EventEmitter from 'es2015-event-emitter';
+import EventEmitter from 'eventemitter3';
 const { ReactNativeAudioStreaming } = NativeModules;
 let NATIVE_INSTANCE_COUNTER = 0;
 let instanceMap = {};
 
 function broadcastToAllInstances(evtName,evtData){
 Object.keys(instanceMap).map((playerId) => {
-  instanceMap[playerId].trigger(evtName,evtData);
+  instanceMap[playerId].emit(evtName,evtData);
 });
 };
 function subscribeGlobalAudioEvents(){
@@ -40,89 +40,89 @@ DeviceEventEmitter.addListener(
 }
 
 class ReactNativeStreamingPlayer extends EventEmitter {
-constructor(soundUrl){
-    super();
-    this._nativeInstanceId = NATIVE_INSTANCE_COUNTER++;
-    this._currentSoundUrl = soundUrl;
-    ReactNativeAudioStreaming.createPlayer(this._nativeInstanceId);
-    instanceMap[this._nativeInstanceId] = this;
-}
-dispatchAudioEvent(evt){
-  this.trigger('stateChange',evt);
-}
-dispatchAudioPlaybackStopEvent(evt){
-  this.trigger('playbackStopped',evt);
-}
-play(){
-  if( this._currentSoundUrl.indexOf('://') > -1 ){
-      ReactNativeAudioStreaming.playWithKey(this._nativeInstanceId,this._currentSoundUrl);
-  } else {
-      ReactNativeAudioStreaming.playLocalWithKey(this._nativeInstanceId,this._currentSoundUrl);
-  }
-}
-pause(){
-  ReactNativeAudioStreaming.pauseWithKey(this._nativeInstanceId);
-}
-resume(){
-  ReactNativeAudioStreaming.resumeWithKey(this._nativeInstanceId);
-}
-stop(){
-  ReactNativeAudioStreaming.stopWithKey(this._nativeInstanceId);
-}
-setVolume(volInt){
-  ReactNativeAudioStreaming.setVolumeWithKey(this._nativeInstanceId,volInt);
-}
-setPan(panInt){
-}
-seekToTime(secondsDouble){
-  ReactNativeAudioStreaming.seekToTimeWithKey(this._nativeInstanceId,secondsDouble);
-}
-goForwardWithKey(secondsDouble){
-  ReactNativeAudioStreaming.goForwardWithKey(this._nativeInstanceId,secondsDouble);
-}
-goBackWithKey(secondsDouble){
-  ReactNativeAudioStreaming.goBackWithKey(this._nativeInstanceId,secondsDouble);
-}
-setSoundUrl(urlString){
-  this._currentSoundUrl = urlString;
-}
-getSoundUrl(){
-  return this._currentSoundUrl;
-}
-getPan(cb){ //@TODO: implement objective-c bridged methods for getters
-}
-getVolume(cb){
-  return ReactNativeAudioStreaming.getVolumeWithKey(this._nativeInstanceId,(err,data) => {
-    cb(err,data.volume);
-  });
-}
-isPaused (cb){
-  ReactNativeAudioStreaming.getStatusWithKey(this._nativeInstanceId,(err,data) => {
-    cb(err,data.status == "PAUSED");
-  });
-}
-isPlaying (cb){
-  ReactNativeAudioStreaming.getStatusWithKey(this._nativeInstanceId,(err,data) => {
-    console.log('callback for getStatus called')
-    cb(err,data.status == "PLAYING");
-  });
-}
-getPosition(cb){
-  ReactNativeAudioStreaming.getStatusWithKey(this._nativeInstanceId,(err,data) => {
-    console.log('callback for getStatus called')
-    cb(err,data.progress,data.duration);
-  });
-}
-getStatus(cb){
-  ReactNativeAudioStreaming.getStatusWithKey(this._nativeInstanceId,cb);
-}
-destroy(){
-  ReactNativeAudioStreaming.destroyWithKey(this._nativeInstanceId);
-  instanceMap[this._nativeInstanceId] = undefined;
-}
-setNowPlayingInfo(trackName,imageName){
-  ReactNativeAudioStreaming.setNowPlayingInfo(trackName,imageName);
-}
+    constructor(soundUrl){
+        super();
+        this._nativeInstanceId = NATIVE_INSTANCE_COUNTER++;
+        this._currentSoundUrl = soundUrl;
+        ReactNativeAudioStreaming.createPlayer(this._nativeInstanceId);
+        instanceMap[this._nativeInstanceId] = this;
+    }
+    dispatchAudioEvent(evt){
+      this.emit('stateChange',evt);
+    }
+    dispatchAudioPlaybackStopEvent(evt){
+      this.emit('playbackStopped',evt);
+    }
+    play(){
+      if( this._currentSoundUrl.indexOf('://') > -1 ){
+          ReactNativeAudioStreaming.playWithKey(this._nativeInstanceId,this._currentSoundUrl);
+      } else {
+          ReactNativeAudioStreaming.playLocalWithKey(this._nativeInstanceId,this._currentSoundUrl);
+      }
+    }
+    pause(){
+      ReactNativeAudioStreaming.pauseWithKey(this._nativeInstanceId);
+    }
+    resume(){
+      ReactNativeAudioStreaming.resumeWithKey(this._nativeInstanceId);
+    }
+    stop(){
+      ReactNativeAudioStreaming.stopWithKey(this._nativeInstanceId);
+    }
+    setVolume(volInt){
+      ReactNativeAudioStreaming.setVolumeWithKey(this._nativeInstanceId,volInt);
+    }
+    setPan(panInt){
+    }
+    seekToTime(secondsDouble){
+      ReactNativeAudioStreaming.seekToTimeWithKey(this._nativeInstanceId,secondsDouble);
+    }
+    goForwardWithKey(secondsDouble){
+      ReactNativeAudioStreaming.goForwardWithKey(this._nativeInstanceId,secondsDouble);
+    }
+    goBackWithKey(secondsDouble){
+      ReactNativeAudioStreaming.goBackWithKey(this._nativeInstanceId,secondsDouble);
+    }
+    setSoundUrl(urlString){
+      this._currentSoundUrl = urlString;
+    }
+    getSoundUrl(){
+      return this._currentSoundUrl;
+    }
+    getPan(cb){ //@TODO: implement objective-c bridged methods for getters
+    }
+    getVolume(cb){
+      return ReactNativeAudioStreaming.getVolumeWithKey(this._nativeInstanceId,(err,data) => {
+        cb(err,data.volume);
+      });
+    }
+    isPaused (cb){
+      ReactNativeAudioStreaming.getStatusWithKey(this._nativeInstanceId,(err,data) => {
+        cb(err,data.status == "PAUSED");
+      });
+    }
+    isPlaying (cb){
+      ReactNativeAudioStreaming.getStatusWithKey(this._nativeInstanceId,(err,data) => {
+        console.log('callback for getStatus called')
+        cb(err,data.status == "PLAYING");
+      });
+    }
+    getPosition(cb){
+      ReactNativeAudioStreaming.getStatusWithKey(this._nativeInstanceId,(err,data) => {
+        console.log('callback for getStatus called')
+        cb(err,data.progress,data.duration);
+      });
+    }
+    getStatus(cb){
+      ReactNativeAudioStreaming.getStatusWithKey(this._nativeInstanceId,cb);
+    }
+    destroy(){
+      ReactNativeAudioStreaming.destroyWithKey(this._nativeInstanceId);
+      instanceMap[this._nativeInstanceId] = undefined;
+    }
+    setNowPlayingInfo(trackName,imageName){
+      ReactNativeAudioStreaming.setNowPlayingInfo(trackName,imageName);
+    }
 }
 subscribeGlobalAudioEvents();
 export default ReactNativeStreamingPlayer;
