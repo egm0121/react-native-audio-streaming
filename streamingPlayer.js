e68953117e84a49dfda4263427536d7e145cecab
Giulio Dellorbo <giulio.dellorbo@8x8.com>
2019-09-12 08:53:46 -0400

    prevent loadSound if null soundUrl specified

--- streamingPlayer.js
+++ streamingPlayer.js
@@ -54,6 +54,9 @@ class ReactNativeStreamingPlayer extends EventEmitter {
       this.emit('playbackStopped',evt);
     }
     play(){
+      if( !this._currentSoundUrl ){
+       return false;
+      }
       if( this._currentSoundUrl.indexOf('://') > -1 ){
           ReactNativeAudioStreaming.playWithKey(this._nativeInstanceId,this._currentSoundUrl);
       } else {
